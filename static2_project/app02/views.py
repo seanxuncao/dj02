97e4fe0ff1bc7e117fed11ea04c8010fb3883c3e
Your Name <you@example.com>
2021-01-11 17:33:10 -0500

    static2 picture

--- static2_project/app02/views.py
+++ static2_project/app02/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {"info": "my name is sean"})
+    return render(request, 'home.html', {"info": "my name is sean, I like python"})
